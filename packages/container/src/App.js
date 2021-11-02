f841555e059172e8e38aa731b0a7cd288eb763d5
Itamar_Kor <itamar_kor@malam-payroll.com>
2021-11-02 17:37:10 +0200

    change App.js in container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import MarketingApp from '../components/MarketingApp';
 export default () => {
     return <div>
-        <h1>MarketingApp</h1>
-        <hr/>
         <MarketingApp />
     </div>;
 }
