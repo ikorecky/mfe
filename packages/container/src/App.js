1d9580d234a0c350fb856955c7bbe3e8fd062f7c
Itamar_Kor <itamar_kor@malam-payroll.com>
2021-11-02 18:25:35 +0200

    update container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MarketingApp from '../components/MarketingApp';
 export default () => {
     return <div>
+        <h1>MFE</h1>
         <MarketingApp />
     </div>;
 }
