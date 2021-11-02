d22785c18ddce7b85886fb29dd780542e168b4fd
Itamar_Kor <itamar_kor@malam-payroll.com>
2021-11-02 17:53:38 +0200

    change container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import MarketingApp from '../components/MarketingApp';
 export default () => {
-    return <div><MarketingApp /></div>;
+    return <div>
+        <MarketingApp />
+    </div>;
 }
