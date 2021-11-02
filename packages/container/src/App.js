f401857270a1717a0a1289ac38da08f1222c2a64
Itamar_Kor <itamar_kor@malam-payroll.com>
2021-11-02 13:02:46 +0200

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import MarketingApp from '../components/MarketingApp';
+export default () => {
+    return <div>
+        <h1>MarketingApp</h1>
+        <hr/>
+        <MarketingApp />
+    </div>;
+}
