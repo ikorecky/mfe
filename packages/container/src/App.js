3a494380f703f6b7fcb8a72e33359754f9d57c4f
Itamar_Kor <itamar_kor@malam-payroll.com>
2021-11-02 19:52:01 +0200

    add header to container deployment

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
 import MarketingApp from '../components/MarketingApp';
+import Header from '../components/Header';
+
 export default () => {
-    return <div>
-        <h1>MFE Project</h1>
-        <MarketingApp />
-    </div>;
+    return (
+        <BrowserRouter>
+            <div>
+                <Header/>
+                <MarketingApp/>
+            </div>
+        </BrowserRouter>
+
+    );
 }
